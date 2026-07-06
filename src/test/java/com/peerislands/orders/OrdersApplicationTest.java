a00d583d2751387bf5bf3c45ce66594a8aa7833d
hemanthsavasere <hemanth.savasere@gmail.com>
2026-07-06 16:46:24 +0000

    chore: add application configs for prod (Postgres) and test (H2)

--- src/test/java/com/peerislands/orders/OrdersApplicationTest.java
+++ src/test/java/com/peerislands/orders/OrdersApplicationTest.java
@@ -2,8 +2,10 @@ package com.peerislands.orders;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class OrdersApplicationTest {
     @Test
     void contextLoads() {
